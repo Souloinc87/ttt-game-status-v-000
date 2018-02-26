87ad0c74415028c5be00e5173bafc5f74a251aba
Marcus  = Soulo <souloinc87@gmail.com>
2018-02-26 22:30:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,7 +43,7 @@ end
 
 def over?(board)
   board.all? do |gameover|
-    if  full?(board) || draw?(board) || won?(board)
+    if  full?(board) || draw?(board) || won?(board) == true
     gameover
     end
   end
