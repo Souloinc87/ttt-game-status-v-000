a06c8b8eecd0452be19071ce495ec06a82ba2d91
Marcus  = Soulo <souloinc87@gmail.com>
2018-02-26 21:20:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ end
 def draw?(board)
   board.each do |draw_board|
     if full?(board) == draw_board
-      return true
+       true
     elsif won?(board) != draw_board
       return false
     end
