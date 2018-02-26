5494d4cb2c3fd021e510041c8b12d19322611264
Marcus  = Soulo <souloinc87@gmail.com>
2018-02-26 22:51:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,8 +19,10 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
+   false
    end
- false
+
+
 end
 
 def full?(board)
